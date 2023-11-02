b67cc83d1e3d3063a6717d9fa807d643470400df
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2023-11-02 17:43:28 -0300

    routers
    agregados routers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,17 @@
 import Carrusel from './Carrusel';
 import About from './About';
+import Footer from './Footer';
+import Stem from './Stem';
+import StemInfo from './StemInfo';
 
 function Home() {
   return (
     <div>
       <Carrusel />
       <About />
+      <Stem />
+      <StemInfo />
+      <Footer />
     </div>
   );
 }
