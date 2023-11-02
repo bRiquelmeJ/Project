d61472d2c423a57a2d39cd75752093a3cd0c377a
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2023-11-02 12:35:37 -0300

    ROUTER Y REGISTRO
    Agregue los links para la pagina home registro y avance el registro , solo faltaria conectarlo a BD y cambiar el diseño

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Carrusel from './Carrusel';
+import About from './About';
+
+function Home() {
+  return (
+    <div>
+      <Carrusel />
+      <About />
+    </div>
+  );
+}
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import logo from "../img/Mascota_01-removebg-preview.png"
-
+import { Link } from 'react-router-dom';
 function Navbar(){
     return(
     <section id="BarraNav">
     <nav className="navbar navbar-dark bg-dark fixed-top">
       <div className="container-fluid">
        <div>
-          <img id="logoNav" src={logo} alt="logo" className="d-inline-block" />
-          <a className="navbar-brand text-white" href="#" id="texto_titulo">EquidApp</a>
+       <img id="logoNav" src={logo} alt="logo" className="d-inline-block" />
+<Link to="/home" className="navbar-brand text-white" id="texto_titulo">EquidApp</Link>
        </div>
         <button id="hamburguesa" className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -19,16 +19,16 @@ function Navbar(){
             <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
+          <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+  <li className="nav-item">
+    <Link to="/home" className="nav-link text-white">Home</Link>
+  </li>
               <li className="nav-item">
                 <a className="nav-link active" href="#">Iniciar sesión</a>
               </li>
               <li className="nav-item">
-                  <a className="nav-link active" href="#">Registrarse</a>
-                </li>
+  <Link to="/registrarse" className="nav-link text-white">Registrarse</Link>
+</li>
                 <li className="nav-item">
                   <a className="nav-link active" href="#">Foro</a>
                 </li>
