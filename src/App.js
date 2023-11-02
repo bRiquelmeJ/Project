a67187bd079ff54e3bc51b0e22e84df35b676ce4
bastian-millanir <bastyok123@gmail.com>
2023-11-02 16:48:39 -0300

    Página inicio :)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from './components/Navbar';
 import Carrusel from './components/Carrusel';
 import './App.css';
 import About from './components/About';
+import Stem from './components/Stem';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <Navbar />
     <Carrusel />
     <About />
+    <Stem />
+    <Footer />
     </div>
   );
 }
