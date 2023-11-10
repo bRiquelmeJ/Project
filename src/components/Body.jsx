db74ccc85fcd4083ac3a0095bb18b7e755a9c677
bastian-millanir <bastyok123@gmail.com>
2023-11-09 23:17:26 -0300

    Matematicas inicio

---
+++ src/components/Body.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import Quiz from '../img/iconos/Quiz.svg';
+import Recursos from '../img/iconos/Recursos.svg';
+import Insignia from '../img/iconos/Insignia.png';
+import Foro from '../img/iconos/Foro.png';
+import Ajustes from '../img/iconos/Ajustes.png';
+import S from '../img/iconos/S.svg';
+import T from '../img/iconos/T.svg';
+import E from '../img/iconos/E.svg';
+import M from '../img/iconos/M.svg';
+import perfil from '../img/perfil.jpg';
+import Body2 from '../components/Body2';
+
+import '../body.css';
+
+function Body() {
+  return (
+    <div className="app-container container-fluid">
+      <div className="row">
+        <div className="app-sidebar col-lg-2 col-md-3 col-sm-4 col-6">
+          <div className="user-box">
+            <img src={perfil} alt="" className="foto-perfil" />
+          </div>
+          <div className="user-box-text">
+            Camila
+          </div>
+          <div className="btn-group-vertical">
+            <div className="row">
+              <div className="col">
+                <img className="icons-btn" src={Quiz} alt="Quiz" />
+                <button type="button" className="btn btn-block">
+                  Quiz
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <img className="icons-btn" src={Recursos} alt="Recursos" />
+                <button type="button" className="btn btn-block">
+                  Recursos
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <img className="col icons-btn" src={Insignia} alt="Insignias" />
+                <button type="button" className="btn btn-block">
+                  Insignia
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <img className="icons-btn" src={Foro} alt="Foro" />
+                <button type="button" className="btn btn-block">
+                  Foro
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <img className="col icons-btn" src={Ajustes} alt="Ajustes" />
+                <button type="button" className="btn btn-block">
+                  Ajustes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="app-main col-lg-10 col-md-9 col-sm-8 col-6">
+          <div className="header-box container">
+            <div className="welcome-box row">
+              <div className="col">
+                <h2>Bienvenid@ de vuelta</h2>
+              </div>
+            </div>
+            <div className="m-4 row">
+              <div className="col">
+                <button className="icons-stem border-0"><img src={S} alt="Icono S" /></button>
+              </div>
+              <div className="col">
+                <button className="icons-stem border-0"><img src={T} alt="Icono T" /></button>
+              </div>
+              <div className="col">
+                <button className="icons-stem border-0"><img src={E} alt="Icono E" /></button>
+              </div>
+              <div className="col">
+                <button className="icons-stem border-0"><img src={M} alt="Icono M" /></button>
+              </div>
+            </div>
+          </div>
+          <Body2 />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
