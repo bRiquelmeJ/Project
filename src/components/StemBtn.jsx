79f9d725ead12dbb2cf8b810ad9015c9dc9df967
bastian-millanir <bastyok123@gmail.com>
2023-11-10 10:23:37 -0300

    Seccion matematicas inicio

--- src/components/StemBtn.jsx
+++ src/components/StemBtn.jsx
@@ -3,34 +3,39 @@ import T from '../img/iconos/T.svg'
 import E from '../img/iconos/E.svg'
 import M from '../img/iconos/M.svg'
 import { Link } from 'react-router-dom'
-import Matematicas from './Matematicas/Matematicas-1'
+import '../pCursos.css'
 
-function StemBtn(){
+function StemBtn() {
+  return (
+    <div className="header-box container mt-5 pb-2">
+      <div className="welcome-box row p-3 w-50">
+        <h2>Bienvenid@ de vuelta</h2>
+      </div>
 
-    return(
-        <div className="header-box container w-60   ">
-          <div className="welcome-box w-50 row ">
-            <div className="welcome-box-text">
-            <h2>Bienvenid@ de vuelta</h2>
-            </div>
-            
-          </div>
-          <div className="  m-4 row ">
-            <div className=" col">
-              <button className='icons-stem border-0'><img src={S} alt="Icono S" /></button>
-            </div>
-            <div className=" col">
-              <button className='icons-stem border-0'><img src={T} alt="Icono T" /></button>
-            </div>
-            <div className=" col">
-              <button className='icons-stem border-0'><img src={E} alt="Icono E" /></button>         
-            </div>
-            <div className=" col">
-              <Link to={Matematicas} className='icons-stem border-0'><img src={M} alt="Icono M" /></Link>
-            </div>
-          </div>
+      <div className="row mt-4">
+        <div className="col">
+          <button className="icons-stem btn rounded-circle">
+            <img src={S} alt="Icono S" />
+          </button>
         </div>
-    )
+        <div className="col">
+          <button className="icons-stem btn rounded-circle">
+            <img src={T} alt="Icono T" />
+          </button>
+        </div>
+        <div className="col">
+          <button className="icons-stem btn rounded-circle">
+            <img src={E} alt="Icono E" />
+          </button>
+        </div>
+        <div className="col">
+          <Link to="/Matematicas" className="icons-stem btn rounded-circle">
+            <img src={M} alt="Icono M" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default StemBtn;
