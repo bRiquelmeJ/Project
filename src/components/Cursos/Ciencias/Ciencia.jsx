774a445aa819568e000dbeaece0d8000e6726960
bastian-millanir <bastyok123@gmail.com>
2023-11-13 11:33:11 -0300

    Agregados botones ciencias

--- src/components/Cursos/Ciencias/Ciencia.jsx
+++ src/components/Cursos/Ciencias/Ciencia.jsx
@@ -1,58 +1,42 @@
-import React from 'react';
-import Sidebar from '../../Cursos/Sidebar';
-import '../../../Styles/ciencias.css';
-import Biologia from '../../../img/BIOLOGIA.png'
-import Quimica from '../../../img/QUIMICA.png'
-import Fisica from '../../../img/FISICA.png'
-
-
-
+import React from "react";
+import Sidebar from "../../Cursos/Sidebar";
+import "../../../Styles/ciencias.css";
+import Biologia from "../../../img/BIOLOGIA.png";
+import Quimica from "../../../img/QUIMICA.png";
+import Fisica from "../../../img/FISICA.png";
 
 function Ciencia() {
+    return (
+        <div className="app-container container-fluid  row">
+            <div className="container-fluid app-sidebar col-lg-10 col-md-12 w-50 h-80">
+                <Sidebar />
+            </div>
+            <div className="app-main container-fluid col-lg col-md-12 w-50 h-80">
+                <div className="welcome-box w-50 row ">
+                    <div className="welcome-box-text">
+                        <h2>Bienvenid@ a Ciencias</h2>
+                    </div>
+                </div>
 
-return (
-    <div className="app-container container-fluid  row">
-    <div className="container-fluid app-sidebar col-lg-10 col-md-12 w-50 h-80">
-    <Sidebar/>
-    </div>
-    <div className="app-main container-fluid col-lg col-md-12 w-50 h-80">
-        <div className="welcome-box w-50 row ">
-            <div className="welcome-box-text">
-            <h2>Bienvenid@ a Ciencias</h2>
+                <div className="mt-3 row d-flex align-items-center justify-content-center">
+                    <div className="btn-group col-lg-4 col-md-6 col-sm-12 d-flex align-items-center justify-content-center">
+                        <button type="button" className="btn btn-lg btn-block">
+                            <img src={Biologia} alt="Biologia" className="img-fluid" />
+                        </button>
+                    </div>
+                    <div className="btn-group col-lg-4 col-md-6 col-sm-12 d-flex align-items-center justify-content-center">
+                        <button type="button" className="btn btn-lg btn-block">
+                            <img src={Quimica} alt="Quimica" className="img-fluid" />
+                        </button>
+                    </div>
+                    <div className="btn-group col-lg-4 col-md-6 col-sm-12 d-flex align-items-center justify-content-center">
+                        <button type="button" className="btn btn-lg btn-block">
+                            <img src={Fisica} alt="Fisica" className="img-fluid" />
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div className="mt-3">
-        <div class="btn-group" role="group" aria-label="iconos">
-            <button 
-            type="button" 
-            class="btn"
-            img src={Biologia} alt="Biologia">
-            </button>
-            <button 
-            type="button" 
-            class="btn"
-            img src={Quimica} alt="Quimica">
-
-            </button>
-            <button 
-            type="button" 
-            class="btn"
-            img src={Fisica} alt="Fisica">
-
-            </button>
-        </div>
-
-        </div>
-    
-
-
-
-    </div>
-    
-    </div>
-
-
-
-)}
+    );
+}
 export default Ciencia;
