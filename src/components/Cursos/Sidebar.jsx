b561b3c9c55e5e0cb9f1362ac89d067548cb36ce
GabiGQ <gigutierrezquezada@gmail.com>
2023-11-13 09:19:22 -0300

    botones sidebar

--- src/components/Cursos/Sidebar.jsx
+++ src/components/Cursos/Sidebar.jsx
@@ -12,19 +12,23 @@ function Sidebar() {
     return (
         <div className="user-box row">
             <div>
+            <div>
                 <img src={perfil} alt="perfil" className="foto-perfil" />
             </div>
-            <div >
+            <div  >
             <h4>Usuario</h4> 
             </div>
+                <div> 
 
-            <ul>
-                <li><img className=' icons-btn' src={Quiz} alt="Quiz" /></li>
-                <li><Link to="/recurso"><img className='icons-btn' src={Recursos} alt="Recursos" /></Link></li>
-                <li><img className='col icons-btn' src={Insignia} alt="Insignias" /></li>
-                <li><img className='icons-btn' src={Foro} alt="Foro" /></li>
-                <li><img className='col icons-btn' src={Ajustes} alt="Ajustes" /></li>
-            </ul>
+                <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+                <button type="button" className="btn"><img className=' icons-btn' src={Quiz} alt="Quiz" /></button>
+                <button type="button" className="btn"><Link to="/recurso"><img className='icons-btn' src={Recursos} alt="Recursos" /></Link></button>
+                <button type="button" className="btn"><img className='col icons-btn' src={Insignia} alt="Insignias" /></button>
+                <button type="button" className="btn"><img className='icons-btn' src={Foro} alt="Foro" /></button>
+                <button type="button" className="btn"><img className='col icons-btn' src={Ajustes} alt="Ajustes" /></button>
+                </div>
+            </div>
+            </div>
         </div>
 
     )
