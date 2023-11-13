87d07adb4728bf160507bd4d5f2cff4bc900a79b
GabiGQ <gigutierrezquezada@gmail.com>
2023-11-13 12:56:49 -0300

    cursos

---
+++ src/components/Cursos/Ingenieria/Robotica.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Sidebar from '../Sidebar';
+import '../../../Styles/ingenieria.css';
+
+
+function Ingenieria() {
+
+  return (
+    <div className="app-container container-fluid  row">
+      <div className="container-fluid app-sidebar col-lg-10 col-md-12">
+        
+        <Sidebar/>
+      </div>
+      
+      <div className="app-main container-fluid col-lg col-md-12 w-50 h-80">
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default Ingenieria;
