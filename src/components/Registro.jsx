9be4823d16591ead18567fa3d9cb6f0f18ebe731
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2023-11-14 15:22:38 -0300

    Update Registro.jsx

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import imagenLogin from "../img/imagen-login.png";
 import '../register.css';
 
-function Registro() {
+export default function Registro() {
+  const [name, setName] = useState('');
+  const [lastName, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [date, setDate] = useState('');
+  
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  
+  const handleLastname = (e) => {
+    setLastname(e.target.value);
+  };
+  
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Previene el comportamiento por defecto del formulario
+    if (name === '' || lastName === '' || email === '' || password === '' || date === '') {
+      alert('Todos los campos son obligatorios');
+      return;
+    }
+    const user = {
+      "name": name,
+      "lastname": lastName,
+      "email": email,
+      "password": password,
+      "date": date,
+    }
+   
+    fetch('http://localhost:8080/api/v1/crear', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+      alert('Usuario creado correctamente');
+    });
+  };
+
   return (
     <section id="about">
       <div className="container mt-4 p-4">
@@ -12,46 +66,32 @@ function Registro() {
             <h3 id="subtituloRegistro" className="text pt-">Regístrate para ser parte <br /> de nuestra comunidad</h3>
             <div className="row">
               <div className="col-md-12">
-                <p className="text-start"></p>
-                <form style={{ width: '80%', margin: 'auto' }}>
+                <form onSubmit={handleSubmit} style={{ width: '80%', margin: 'auto' }}> {/* Agregado onSubmit aquí */}
                   <div className="row pt-2 pb-3">
                     <div className="col">
-                      <input type="text" className="form-control" placeholder="Nombre" aria-label="First name" />
+                      <input type="text" className="form-control" placeholder="Nombre" aria-label="First name" onChange={handleName} />
                     </div>
                     <div className="col">
-                      <input type="text" className="form-control" placeholder="Apellido" aria-label="Last name" />
+                      <input type="text" className="form-control" placeholder="Apellido" aria-label="Last name" onChange={handleLastname} />
                     </div>
                   </div>
                   <div className="row">
                     <h6>Fecha de Nacimiento</h6>
                   </div>
                   <div className="row p-3">
-                    <input type="date" />
+                    <input type="date" onChange={handleDate}/>
                   </div>
                   <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Correo</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text"></div>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleEmail} />
                   </div>
                   <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <input type="password" className="form-control" id="exampleInputPassword1" onChange={handlePassword} />
                   </div>
-                  <button id="btnRegistro" type="submit" className="btn btn-purple">Registrarse</button>
-                  <div>
-                  <button className="btn btn-light border">
-    <i className="fab fa-google"></i> Registrate con Google
-</button>
-<div>
-                  <button className="btn btn-light border">
-    <i className="fab fa-google"></i> Registrate con Facebook
-</button>
-
-</div>
-</div>
+                  <button type="submit" className="btn btn-purple">Registrarse</button>
                 </form>
               </div>
-              
             </div>
           </div>
           <div className="col-lg-4 col-md-12">
@@ -64,5 +104,3 @@ function Registro() {
     </section>
   );
 }
-
-export default Registro;
