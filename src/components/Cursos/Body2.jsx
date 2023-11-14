c22a8a3e0e888eee84fed93bee9e82e311c647bb
GabiGQ <gigutierrezquezada@gmail.com>
2023-11-14 00:10:16 -0300

    aa

--- src/components/Cursos/Body2.jsx
+++ src/components/Cursos/Body2.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../Styles/pCursos.css';
 import MujerSTEM from "../../img/STEMimg.jpg";
 import { Link } from 'react-router-dom';
