915e02388ac9a48c493a5453083d44f8da0ef474
Carlos Enrique Navarro Miranda <146015240+K4rlos96@users.noreply.github.com>
2023-11-15 18:50:06 -0300

    avatarPerson
    cambios agregados

--- src/components/Cursos/Sidebar.jsx
+++ src/components/Cursos/Sidebar.jsx
@@ -1,39 +1,64 @@
-import Quiz from '../../img/iconos/Quiz.svg';
-import Recursos from '../../img/iconos/Recursos.svg';
-
-import Foro from '../../img/iconos/Foro.png'
-import Ajustes from '../../img/iconos/Ajustes.png'
-import perfil from '../../img/perfil.jpeg'
-import '../../Styles/pCursos.css';
+// Sidebar.js
+import React, { useState } from 'react';
+import AvatarModal from './Avatar/AvatarModal'; // Verifica la ruta
+import perfil from '../../img/perfil.jpeg';
+import '../../Styles/pCursos.css'; // Verifica la ruta
 import { Link } from 'react-router-dom';
 
 function Sidebar() {
+    const [isAvatarModalOpen, setAvatarModalOpen] = useState(false);
+    const [avatar, setAvatar] = useState(() => {
+        // Intenta obtener el avatar del almacenamiento local o usa el predeterminado
+        return localStorage.getItem('userAvatar') || perfil;
+    });
+
+    const handleAvatarChange = (newAvatarSrc) => {
+        setAvatar(newAvatarSrc);
+        localStorage.setItem('userAvatar', newAvatarSrc);
+        setAvatarModalOpen(false);
+    };
 
     return (
         <div className="user-box row">
             <div>
-            <div>
-            <Link to="/PlataformaCursos">
-      <img src={perfil} alt="perfil" className="foto-perfil" />
-    </Link>
+                <img src={avatar} alt="perfil" className="foto-perfil" />
+                <button className="bookmarkBtn" onClick={() => setAvatarModalOpen(true)}>
+                    <span className="IconContainer">
+                        {/* Agrega aquí el SVG o ícono para "Cambiar Avatar" */}
+                    </span>
+                    <p className="text">Cambiar Avatar</p>
+                </button>
 
+                <AvatarModal
+                    isOpen={isAvatarModalOpen}
+                    onClose={() => setAvatarModalOpen(false)}
+                    onSelectAvatar={handleAvatarChange}
+                />
             </div>
-            <div >
-            <h4 className='perfilFont'>Usuario</h4> 
-            </div>
-                <div> 
-
-                <div class="btn-group-vertical sidebar" role="group" aria-label="Vertical button group">
-                <button type="button" className="btn "><Link to="/quizz-intro"><img className=' icons-btn' src={Quiz} alt="Quiz" /></Link></button>
-                <button type="button" className="btn"><Link to="/recurso"><img className='icons-btn' src={Recursos} alt="Recursos" /></Link></button>
-                <button type="button" className="btn"><img className='icons-btn' src={Foro} alt="Foro" /></button>
-                <button type="button" className="btn"><img className='col icons-btn' src={Ajustes} alt="Ajustes" /></button>
-                </div>
-            </div>
+            <div className="btn-group-vertical sidebar" role="group" aria-label="Vertical button group">
+                <button type="button" className="bookmarkBtn">
+                    <Link to="/quizz-intro">
+                        <p className="text">Quiz</p>
+                    </Link>
+                </button>
+                <button type="button" className="bookmarkBtn">
+                    <Link to="/recurso">
+                        <p className="text">Recursos</p>
+                    </Link>
+                </button>
+                <button type="button" className="bookmarkBtn">
+                    <Link to="/foro">
+                        <p className="text">Foro</p>
+                    </Link>
+                </button>
+                <button type="button" className="bookmarkBtn">
+                    <Link to="/ajustes">
+                        <p className="text">Ajustes</p>
+                    </Link>
+                </button>
             </div>
         </div>
-
-    )
+    );
 }
 
 export default Sidebar;
