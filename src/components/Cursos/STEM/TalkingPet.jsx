4464abf7a9100068d818aadb325a7679fcb4fd86
bRiquelmeJ <147190608+bRiquelmeJ@users.noreply.github.com>
2023-11-16 09:06:18 -0300

    segundo intento

--- src/components/Cursos/STEM/TalkingPet.jsx
+++ src/components/Cursos/STEM/TalkingPet.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../../../Styles/pCursos.css';
 import Mascota from '../../../img/Logo EquidApp.png';
-import IntroStem from '../Contenido/IntroStem.json';
 
 function TalkingPet() {
   const [paragraphs, setParagraphs] = useState([]);
@@ -9,12 +8,17 @@ function TalkingPet() {
 
   useEffect(() => {
     // Cargar datos desde el archivo JSON
-    fetch(IntroStem)
-      .then(response => response.json())
-      .then(data => setParagraphs(data.paragraphs))  // Ajusta según la estructura real del JSON
+    fetch(Intro)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error de red: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(paragraphs => setParagraphs(paragraphs))
       .catch(error => console.error('Error al cargar el archivo JSON:', error));
   }, []);
-
+  
   function showNextParagraph() {
     if (currentIndex < paragraphs.length - 1) {
       setCurrentIndex(currentIndex + 1);
@@ -26,11 +30,8 @@ function TalkingPet() {
       <div className="row">
         <div className="col-lg-8 mx-auto h-100" onClick={showNextParagraph}>
           <div className="talkingPetText">
-            {paragraphs.map((paragraph, index) => (
-              <p key={index} style={{ display: index === currentIndex ? 'block' : 'none' }}>
-                {paragraph}
-              </p>
-            ))}
+           
+           
           </div>
         </div>
         <div className="col-lg-4 d-flex justify-content-end align-items-center">
