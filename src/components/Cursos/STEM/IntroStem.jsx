23b0eb0927c0e4d566d9d106b7222c07a4aa412a
bRiquelmeJ <147190608+bRiquelmeJ@users.noreply.github.com>
2023-11-16 11:05:05 -0300

    Cambio

--- src/components/Cursos/STEM/IntroStem.jsx
+++ src/components/Cursos/STEM/IntroStem.jsx
@@ -8,7 +8,6 @@ function IntroStem() {
   return (
     <div className="app-container container-fluid  row">
       <div className="container-fluid app-sidebar col-lg-10 col-md-12">
-        
         <Sidebar/>
       </div>
       
--- src/components/Cursos/STEM/TalkingPet.jsx
+++ src/components/Cursos/STEM/TalkingPet.jsx
@@ -1,37 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../../../Styles/pCursos.css';
 import Mascota from '../../../img/Logo EquidApp.png';
+import ContenidoStem from './ContenidoStem';
 
 function TalkingPet() {
-  const [paragraphs, setParagraphs] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    // Cargar datos desde el archivo JSON
-    fetch(Intro)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Error de red: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(paragraphs => setParagraphs(paragraphs))
-      .catch(error => console.error('Error al cargar el archivo JSON:', error));
-  }, []);
-  
-  function showNextParagraph() {
-    if (currentIndex < paragraphs.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-    }
-  }
+
 
   return (
     <div className="container text-center">
       <div className="row">
-        <div className="col-lg-8 mx-auto h-100" onClick={showNextParagraph}>
+        <div className="col-lg-8 mx-auto h-100">
           <div className="talkingPetText">
-           
-           
+           <ContenidoStem/>
           </div>
         </div>
         <div className="col-lg-4 d-flex justify-content-end align-items-center">
