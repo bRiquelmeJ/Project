fcc2bfb7466b32062ef0ac3dcdef0d4c9ed46270
GabiGQ <gigutierrezquezada@gmail.com>
2023-11-22 19:58:49 -0300

    balbal

---
+++ src/components/Cursos/Ingenieria/Robotica/ImagenesRobotica.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import '../../../../Styles/pCursos.css';
+import img1 from '../../../../img/contenido/biologia.jpg';
+
+
+const ImagenesRobotica= ({ currentIndex, changePoints }) => {
+  const [images] = useState([
+    img1,
+    
+    // ... otras imágenes
+  ]);
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    // Cambiar automáticamente la imagen solo en los puntos de cambio
+    if (Array.isArray(changePoints) && changePoints.includes(currentIndex)) {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }
+  }, [currentIndex, changePoints, images]);
+
+  return (
+    <div className="image-carousel">
+      <img
+        src={images[currentImage]}
+        alt={`Imagen ${currentImage + 1}`}
+        className="img-fluid insideBox" 
+      />
+    </div>
+  );
+};
+
+export default ImagenesRobotica;
