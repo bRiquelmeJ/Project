6c6049dd4d76ea25ab9dee94b4faf886eafbc8ae
GabiGQ <gigutierrezquezada@gmail.com>
2023-11-22 12:01:28 -0300

    info

--- src/components/Cursos/Ciencias/BiologiaContenidoInfo.jsx
+++ src/components/Cursos/Ciencias/BiologiaContenidoInfo.jsx
@@ -1,14 +1,22 @@
+import React from 'react';
+import Biologia from '../../../pdf/Recurso de Biología copia.pdf'
 
 
 export default function ModalContentInfo({ onClose }) {
-
-    return (
-    
+return (
     <div className="modal-1">
 
-        <h1>Info Biologia</h1>
+            <iframe
+                title="PDF Viewer"
+                src={Biologia}
+                width="100%"
+                height="500px"
+            >
+                
+            </iframe>
+
 
         <button onClick={onClose}  type="button" className="btn-close" aria-label="Close"></button>
-    </div>
+</div>
     );
 }
