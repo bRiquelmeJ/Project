3ddb6709ca12e8d8d856d4a5d9686ad1808f54d7
bastian-millanir <bastyok123@gmail.com>
2023-11-22 15:40:03 -0300

    modal info arreglado (un poquito)

--- src/components/Cursos/Ciencias/BiologiaContenidoInfo.jsx
+++ src/components/Cursos/Ciencias/BiologiaContenidoInfo.jsx
@@ -2,31 +2,37 @@ import React from 'react';
 import Biologia from '../../../pdf/Recurso de Biología.docx.pdf'
 import { useState } from 'react';
 import {Document, Page} from "react-pdf"
+import "../../../Styles/ciencias.css";
+
 
 export default function ModalContentInfo({ onClose }) {
-    const [numPages, setNumPages] = useState();
-    function onDocumentLoadSuccess({numPages}) {
-        setNumPages(numPages);
-    }
-return (
+  const [numPages, setNumPages] = useState();
 
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
     <div className="modalpdf">
-        <div className="pdf-div">
-            <Document file={Biologia} onLoadSuccess={onDocumentLoadSuccess}>
-                {Array.apply(null, Array(numPages))
-                .map((x,i)=> i+1).map((page)=> {
-                    return (
-                        <Page key={page} pageNumber={page} renderTextLayer={false} renderAnnotationLayer= {false} />
-                    );
-                })
-                }
-            </Document>
-
-        </div>
-
-
-
-        <button onClick={onClose}  type="button" className="btn-close" aria-label="Close"></button>
-</div>
-    );
+      <div className="pdf-div">
+        <Document file={Biologia} onLoadSuccess={onDocumentLoadSuccess}>
+          {Array.apply(null, Array(numPages)).map((x, i) => i + 1).map((page) => {
+            return (
+              <Page
+                key={page}
+                pageNumber={page}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+                width={800} // Ajusta según sea necesario
+                customTextRenderer={(textItem) => (
+                  <div style={{ fontSize: '30px', lineHeight: '1.5' }}>{textItem.str}</div>
+                )}
+              />
+            );
+          })}
+        </Document>
+      </div>
+      <button onClick={onClose} type="button" className="btn-close" aria-label="Close"></button>
+    </div>
+  );
 }
