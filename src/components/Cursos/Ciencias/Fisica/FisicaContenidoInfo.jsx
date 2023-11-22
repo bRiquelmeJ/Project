96ae26a6837b283dae17b1d4eae2738e94bdcd80
GabiGQ <gigutierrezquezada@gmail.com>
2023-11-22 19:33:46 -0300

    cambio

--- src/components/Cursos/Ciencias/Fisica/FisicaContenidoInfo.jsx
+++ src/components/Cursos/Ciencias/Fisica/FisicaContenidoInfo.jsx
@@ -1,9 +1,8 @@
 
 import React from 'react';
-import Quimica from '../../../pdf/Recurso Quimica.docx.pdf'
+import Fisica from '../../../../pdf/Recurso de Fisica.docx.pdf'
 import { useState } from 'react';
 import {Document, Page} from "react-pdf"
-
 export default function ModalContentInfo({ onClose }) {
     const [numPages, setNumPages] = useState();
     function onDocumentLoadSuccess({numPages}) {
@@ -12,17 +11,18 @@ export default function ModalContentInfo({ onClose }) {
     
     <div className="modalpdf">
     <div className="pdf-div">
-        <Document file={Quimica} onLoadSuccess={onDocumentLoadSuccess}>
-            {Array.apply(null, Array(numPages))
-            .map((x,i)=> i+1).map((page)=> {
-                return (
-                    <Page key={page} pageNumber={page} renderTextLayer={false} renderAnnotationLayer= {false} />
-                );
-            })
-            }
-        </Document>
+            <Document file={Fisica} onLoadSuccess={onDocumentLoadSuccess}>
+                {Array.apply(null, Array(numPages))
+                .map((x,i)=> i+1).map((page)=> {
+                    return (
+                        <Page key={page} pageNumber={page} renderTextLayer={false} renderAnnotationLayer= {false} />
+                    );
+                })
+                }
+            </Document>
 
-    </div>
+        </div>
+
         <button onClick={onClose}  type="button" className="btn-close" aria-label="Close"></button>
     </div>
     );
