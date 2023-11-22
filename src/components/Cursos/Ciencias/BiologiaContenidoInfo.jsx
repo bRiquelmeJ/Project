be4c602460bc5f050eeff7377cba6f83e1f678a5
GabiGQ <gigutierrezquezada@gmail.com>
2023-11-22 12:16:04 -0300

    mm

--- src/components/Cursos/Ciencias/BiologiaContenidoInfo.jsx
+++ src/components/Cursos/Ciencias/BiologiaContenidoInfo.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import Biologia from '../../../pdf/Recurso de Biología copia.pdf'
+import { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
 
 
 export default function ModalContentInfo({ onClose }) {
+    const [numPages, setNumPages] = useState();
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({numPages}) {
+    setNumPages(numPages);
+    }
+
 return (
     <div className="modal-1">
+        <div>
+    <Document file="/src/pdf/Recurso de Biología copia.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+    </Document>
+    <p>
+        Page {pageNumber} of {numPages}
+    </p>
+    </div>
 
-            <iframe
-                title="PDF Viewer"
-                src={Biologia}
-                width="100%"
-                height="500px"
-            >
-                
-            </iframe>
 
 
         <button onClick={onClose}  type="button" className="btn-close" aria-label="Close"></button>
