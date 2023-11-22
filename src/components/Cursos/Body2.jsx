a14ba4220c489c0092c891c29553f6c439444551
bRiquelmeJ <147190608+bRiquelmeJ@users.noreply.github.com>
2023-11-22 14:34:32 -0300

    Link a Intro

--- src/components/Cursos/Body2.jsx
+++ src/components/Cursos/Body2.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import '../../Styles/pCursos.css';
 import MujerSTEM from "../../img/generales/STEMimg.jpg";
 import { Link } from 'react-router-dom';
-
+import IntroStemImage from "../../img/generales/IntroStemImage.jpg";
 
 
 function Body2(){
 
 return(
-
     <div class="container-flex text-center">
-                
-            <div>
-                    <>
-                    <Link to="/MujeresSTEM">
-                        <img 
-                        src={MujerSTEM} 
-                        alt="Mujeres en STEM" 
-                        className="img-mujeres-stem img-fluid imgZoom" />
-                        </Link>  
-                    </>
+
+        <Link to="/IntroStem">
+                    <img 
+                    src={IntroStemImage} 
+                    alt="Intro Stem" 
+                    className="img-mujeres-stem img-fluid imgZoom" />
+                </Link>
+
+                <Link to="/MujeresSTEM">
+                    <img 
+                    src={MujerSTEM} 
+                    alt="Mujeres en STEM" 
+                    className="img-mujeres-stem img-fluid imgZoom" />
+                </Link>  
             </div>
-        </div>
+
 )}
 export default Body2;
